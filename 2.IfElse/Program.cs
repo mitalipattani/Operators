05f6fe87ad48422a6a5b412e86e0f846327fa3bd
Mitali Pattani <sonarmitali19@gmail.com>
2017-09-20 11:51:37 -0700

    Add project files.

---
+++ 2.IfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _2.IfElse
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
